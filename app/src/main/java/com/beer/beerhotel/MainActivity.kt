ecd5e952ddc3f6528eab61b92d95ceb3e62befca
jiangyuanyuan <jiangyuanyuan@gmail.com>
2019-02-07 16:53:15 +0800

    高德地图 简单集成

--- app/src/main/java/com/beer/beerhotel/MainActivity.kt
+++ app/src/main/java/com/beer/beerhotel/MainActivity.kt
@@ -2,11 +2,41 @@ package com.beer.beerhotel
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import com.amap.api.maps.AMap
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mMapView.onCreate(savedInstanceState)
+        val aMap = mMapView.map
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        mMapView.onDestroy()
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+
+        mMapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mMapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mMapView.onSaveInstanceState(outState)
     }
 }
